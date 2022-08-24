9f525b2b6b1e7c5eaa09b26f7eee57d142c9a965
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2022-08-24 11:06:25 -0600

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# eclass-roster-parser
+# Eclass Roster Parser
+
+## Features
+- Exports the student's name, ccid, email, lab enrollment, and lecture enrollment to a CSV file.
+- Excludes instructors and teaching assistants from the output
+- The last row shows the total number of students on the list.
+
+## How to use
+1. Click the **Participants** tab.
+2. At the bottom of the **Participants** page, click **Show all #**.
+3. Right-click on the page and select **Inspect**.
+4. Go to the **Console** tab.
+5. Paste the contents of **parse.js** and hit **enter**.
+6. A **CSV** file will automatically download.
---
+++ parse.js
@@ -0,0 +1,60 @@
+const csv_rows = [
+    // Headers
+    ["Student", "CCID", "Email", "Lab Enrollment", "Lecture Enrollment"]
+];
+
+// Set it to be a CSV file
+let csvContent = "data:text/csv;charset=utf-8,";
+
+// Excludes empty rows
+const rows = document.querySelectorAll('tr[id^="user-index-participants-"][class=""]');
+
+rows.forEach(row => {
+    // Excludes TAs and Instructors
+    if (row.childNodes[4].innerText === "Student"){
+
+    // student name
+    student = row.childNodes[1].innerText;
+    
+    // campus computing id
+    ccid = row.childNodes[2].innerText;
+    
+    // email address
+    email = row.childNodes[3].innerText;
+    
+    // td element with 2 div children
+    enrolled = row.childNodes[7];
+
+    // first child is lab
+    lab = enrolled.childNodes[0];
+    
+    // excludes the day and time
+    lab_enrolled = lab.getAttribute("data-timeenrolled").split(",")[1];
+    
+    // second child is lecture
+    lecture = enrolled.childNodes[1];
+    
+    // excludes the day and time
+    lecture_enrolled = lecture.getAttribute("data-timeenrolled").split(",")[1];
+
+    // append to csv_rows
+    csv_rows.push([student, ccid, email, lab_enrolled, lecture_enrolled]);
+    }
+});
+
+csv_rows.forEach(function(rowArray) {
+    // join them with commas, returns an array as a string
+    let row = rowArray.join(",");
+
+    // add it to the csv file
+    csvContent += row + "\r\n";
+});
+
+// add the number of students to the csv file
+csvContent += `Number of Students,${csv_rows.length-1}`
+
+// Used to encode Uniform Resource Identifiers (URIs) by replacing certain characters by one, two, three or four escape sequences representing the UTF-8 encoding of the character.
+var encodedUri = encodeURI(csvContent);
+
+// Loads the CSV into the existing browser context, automatically downloads the file
+window.open(encodedUri);
